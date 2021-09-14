9ce71ffb1e135c2cba11cb11b5c94a890c141076
Emily Harber <thecodepixi@users.noreply.github.com>
2021-09-14 18:04:15 -0400

    add oh-my-zsh stuff

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh ~/dotfiles/oh-my-zsh.sh 
+
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
