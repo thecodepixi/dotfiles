5ee02e0f80f217eb2dcb5f88f07c4667ed2e164b
Emily Harber <thecodepixi@users.noreply.github.com>
2021-09-14 18:22:47 -0400

    maybe?

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh ~/dotfiles/.oh-my-zsh/oh-my-zsh.sh 
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
+cp -r ~/dotfiles/.oh-my-zsh
