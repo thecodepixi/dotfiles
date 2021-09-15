8f70a8c63f8e5a8f113bf5305db60efa4621269b
Emily Harber <emily.a.harber@gmail.com>
2021-09-15 09:54:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.gitconfig ~/.gitconfig
-cp -r ~/dotfiles/.oh-my-zsh
+cp -r ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
